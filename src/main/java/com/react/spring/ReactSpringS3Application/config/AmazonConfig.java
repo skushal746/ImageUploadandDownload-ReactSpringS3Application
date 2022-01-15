472cebd15222038b64c3ff8967ad480686e05dd0
Kushal Sharma <skushal746@gmail.com>
2022-01-14 20:28:00 -0500

    Update AmazonConfig.java

--- src/main/java/com/react/spring/ReactSpringS3Application/config/AmazonConfig.java
+++ src/main/java/com/react/spring/ReactSpringS3Application/config/AmazonConfig.java
@@ -16,8 +16,8 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "Enter the access key received from AWS",
-                "Enter the secret key received from AWS"
+                "",//"Enter the access key received from AWS"
+                ""//"Enter the secret key received from AWS"
         );
 
         return AmazonS3ClientBuilder
